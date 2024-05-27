bf824b37f578caadfac0604508e6702082c95e54
Harshit <harshit0580.be21@chitkara.edu.in>
2024-05-28 01:08:00 +0530

    site

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const port=process.env.PORT || 5000
 const cors=require('cors')
-const allowedOrigins = ["https://bookie-n0je.onrender.com/"];
+const allowedOrigins = ["https://bookie1.onrender.com"];
 
 app.use(cors({
   origin: (origin, callback) => {
